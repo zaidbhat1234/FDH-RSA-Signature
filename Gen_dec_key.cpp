25714e25018f2fb01605938f5b4e26ce66e39978
zaidbhat1234 <33365681+zaidbhat1234@users.noreply.github.com>
2020-07-04 20:31:03 +0530

    Update Gen_dec_key.cpp

--- Gen_dec_key.cpp
+++ Gen_dec_key.cpp
@@ -57,28 +57,19 @@ mpz_class gcdExtended1(mpz_class a, mpz_class b, mpz_class *x, mpz_class *y)
 }
 
 mpz_class Gen_dec_key( FILE * ptr1,FILE *ptr2){
-    
-    
-    
     mpz_class d;
      mpz_class x,y;
-    
     int length;
-    
     FILE *temp = fopen("Primep.txt", "r");
-    
     fscanf(temp, "%d",&length);
-   
     char p_[length];
     fscanf(temp, "%s",p_);
          mpz_class p;
-     //
     std::stringstream ss;
     ss<<p_;
     ss>>p;
    // p=p_;
     fclose(temp);
-  
     temp = fopen("Primeq.txt", "r");
     fscanf(temp, "%d",&length);
     char q_[length];
@@ -88,35 +79,27 @@ mpz_class Gen_dec_key( FILE * ptr1,FILE *ptr2){
     ss1<<q_;
     ss1>>q;
   //  q=q_;
-    
     mpz_class phi;
     phi = (p-1)*(q-1);
     mpz_class b =2 ;
-    
     mpz_class gcd = gcdExtended1(b,phi,&x,&y);
     while(gcd!=1)
     {
         b++;
         gcd = gcdExtended1(b,phi,&x,&y);
     }
-    
     mpz_class a = x;
     if(a<0){
         a=a+ phi;}
-    
     string temp_f = b.get_str();
     char temp_b[temp_f.length()];
     strcpy(temp_b, temp_f.c_str());
     fprintf(ptr1, "%ld %s ",temp_f.length(),temp_b);
-    
     temp_f = a.get_str();
     char temp_a[temp_f.length()];
     strcpy(temp_a, temp_f.c_str());
     fprintf(ptr2, "%ld %s ",temp_f.length(),temp_a);
-    
     fclose(ptr1);
     fclose(ptr2);
+    return d;}
     
-    return d;
-    
-}
